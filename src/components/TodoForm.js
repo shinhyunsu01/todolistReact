a5dcabff93370fe89e9cafa042f9f857233b1352
신현수 <sinhyeonsu@MacBook-Pro.local>
2022-02-05 13:25:05 +0900

    todo add button console test

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 
-function TodoForm() {
+function TodoForm(props) {
   const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+    });
+  };
+
   return (
-    <form className="todo-form">
+    <form className="todo-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Add a todo"
         value={input}
         name="text"
-        className="toddo-input"
+        className="todo-input"
+        onChange={handleChange}
       />
+      <button className="todo-button">Add todo</button>
     </form>
   );
 }
